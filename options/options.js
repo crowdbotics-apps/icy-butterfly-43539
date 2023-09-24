6a5d2b536852abf4beb06c6e8f3744616008a241
Crowdbotics <team@crowdbotics.com>
2023-09-24 17:45:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-butterfly-43539",
+  url: "https://icy-butterfly-43539.botics.co",
+  api: "https://icy-butterfly-43539.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
